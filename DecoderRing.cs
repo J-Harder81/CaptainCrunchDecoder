a6d455c463b4b18dbaf44109bf4c19e3bc9c9caf
Justi <Justi@HYPER_Z-FURY>
2024-11-19 23:24:24 -0800

    Add project files.

---
+++ DecoderRing.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainCrunchDecoder
+{
+    internal class DecoderRing
+    {
+        public static string Encode(string input)
+        {
+            char[] encoded = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (char.IsLower(c))
+                {
+                    encoded[i] = (char)((c - 'a' + 13) % 26 + 'a');
+                }
+                else if (char.IsUpper(c))
+                {
+                    encoded[i] = (char)((c - 'A' + 13) % 26 + 'A');
+                }
+                else
+                {
+                    encoded[i] = c;
+                }
+            }
+
+            return new string(encoded);
+        }
+        public static string Decode(string input, int shift)
+        {
+            char[] decoded = new char[input.Length];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+
+                if (char.IsLower(c))
+                {
+                    decoded[i] = (char)((c - 'a' + shift + 26) % 26 + 'a');
+                }
+                else if (char.IsUpper(c))
+                {
+                    decoded[i] = (char)((c - 'A' + shift + 26) % 26 + 'A');
+                }
+                else
+                {
+                    decoded[i] = c;
+                }
+            }
+            return new string(decoded);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptainCrunchDecoder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a message to encode:");
+            string input = Console.ReadLine();
+            string encoded = DecoderRing.Encode(input);
+            Console.WriteLine(encoded);
+
+            //With generalization
+            Console.WriteLine("Enter a new message to encode:");
+            string input2 = Console.ReadLine();
+            string encoded2 = DecoderRing.Decode(input2, 13);
+            Console.WriteLine("Your message encoded: " + encoded2);
+            string encoded3 = DecoderRing.Decode(encoded2, -13);
+            Console.WriteLine("Your message decoded: " + encoded3);
+        }
+    }
+}
